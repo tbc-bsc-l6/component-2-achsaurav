023cf634452864fc2df5555a2d206c9ce411482e
saurav acharya <sau8620@gmail.com>
2021-12-24 22:06:44 +0545

    made login and register page with tailwind css framework

---
+++ comp2/resources/views/components/header.blade.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+    <!-- <link rel="stylesheet" href="/style.css"> -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+
+    <nav class="flex h-[80px] w-[100%] items-center justify-between p-[0_50px_0_100px] flex-wrap">
+        <div class="text-[35px] font-semibold">
+           store
+        </div>
+        
+        <ul class="flex flex-wrap list-none	">
+            <li class="m-[0_5px]"><a class="no-underline text-[18px] font-medium p-[8px_15px] rounded-[5px] tracking-[1px] transition-all delay-[0.3s]" href="/">HOME</a></li>
+           <li class="m-[0_5px]"><a class="no-underline text-[18px] font-medium p-[8px_15px] rounded-[5px] tracking-[1px] transition-all delay-[0.3s]" href="#">BOOK</a></li>
+           <li class="m-[0_5px]"><a href="#" class="no-underline text-[18px] font-medium p-[8px_15px] rounded-[5px] tracking-[1px] transition-all delay-[0.3s]">CD</a></li>
+           <li class="m-[0_5px]"><a href="#" class="no-underline text-[18px] font-medium p-[8px_15px] rounded-[5px] tracking-[1px] transition-all delay-[0.3s]">GAME</a></li>
+           <li class="m-[0_5px]"><a href="/register" class="no-underline text-[18px] font-medium p-[8px_15px] rounded-[5px] tracking-[1px] transition-all delay-[0.3s]">REGISTER</a></li>
+        </ul>
+    </nav>
+
+    
+    {{$slot}}
+</body>
+</html>
---
+++ comp2/resources/views/register.blade.php
@@ -0,0 +1,50 @@
+<x-header>
+    <div class="md:max-w-[60%] md:m-auto md:text-[.9em]">
+        
+        <form action="" method="post" >
+            <fieldset class="mb-[30px] border-none">
+                <legend class="text-[2.4em] font-medium mb-[30px] odd:pt-[40px] text-center">Contact Register</legend>
+
+                <div class="md:flex">
+                    <label for="name" class="md:flex-[1_200px] md:max-w-[150px] md:pb-[30px] m-[auto_0] block mb-[8px] text-[15px] font-medium"> Full Name </label>
+                    <input required type="text" class="md:flex-[2_550px] md:max-w-[100%] text-[16px] w-[100%] rounded-[5px] shadow-[0_1px_0_rgba(0,0,0,0.03)] h-auto m-0 outline-0 p-[15px] mb-[30px] border-none opacity-50 focus:border-[2px] focus:border-solid focus:border-black opacity-100 placeholder-shown:text-right" id="name" name="user_name" placeholder="Required">
+                </div>
+
+                <div class="md:flex">
+                    <label for="mail" class="md:flex-[1_200px] md:max-w-[150px] md:pb-[30px] m-[auto_0] block mb-[8px] text-[15px] font-medium"> Email Address </label>
+                    <input required type="email" class="md:flex-[2_550px] md:max-w-[100%] text-[16px] w-[100%] rounded-[5px] shadow-[0_1px_0_rgba(0,0,0,0.03)] h-auto m-0 outline-0 p-[15px] mb-[30px] border-none opacity-50 focus:border-[2px] focus:border-solid focus:border-black opacity-100 placeholder-shown:text-right" id="mail" name="user_email" placeholder="Required">
+                </div>
+
+                <div class="md:flex">
+                    <label for="tel" class="md:flex-[1_200px] md:max-w-[150px] md:pb-[30px] m-[auto_0] block mb-[8px] text-[15px] font-medium"> Password </label>
+                    <input type="tel" class="md:flex-[2_550px] md:max-w-[100%] text-[16px] w-[100%] rounded-[5px] shadow-[0_1px_0_rgba(0,0,0,0.03)] h-auto m-0 outline-0 p-[15px] mb-[30px] border-none opacity-50 focus:border-[2px] focus:border-solid focus:border-black opacity-100 " id="pass" name="user_pass">
+                </div>
+
+
+                <div class="md:flex">
+                    <label for="address" class="md:flex-[1_200px] md:max-w-[150px] md:pb-[30px] m-[auto_0] block mb-[8px] text-[15px] font-medium"> Phone Number </label>
+                    <input type="text" class="md:flex-[2_550px] md:max-w-[100%] text-[16px] w-[100%] rounded-[5px] shadow-[0_1px_0_rgba(0,0,0,0.03)] h-auto m-0 outline-0 p-[15px] mb-[30px] border-none opacity-50 focus:border-[2px] focus:border-solid focus:border-black opacity-100 " id="tel" name="user_tel">
+                </div>
+
+
+                <div class="md:flex">
+                    <label for="city" class="md:flex-[1_200px] md:max-w-[150px] md:pb-[30px] m-[auto_0] block mb-[8px] text-[15px] font-medium"> City </label>
+                    <input type="text" class="md:flex-[2_550px] md:max-w-[100%] text-[16px] w-[100%] rounded-[5px] shadow-[0_1px_0_rgba(0,0,0,0.03)] h-auto m-0 outline-0 p-[15px] mb-[30px] border-none opacity-50 focus:border-[2px] focus:border-solid focus:border-black opacity-100" id="city" name="user_city">
+                </div>
+
+                <div class="md:flex">
+                    <label for="state" class="md:flex-[1_200px] md:max-w-[150px] md:pb-[30px] m-[auto_0] block mb-[8px] text-[15px] font-medium"> State </label>
+                    <input type="text" class="md:flex-[2_550px] md:max-w-[100%] text-[16px] w-[100%] rounded-[5px] shadow-[0_1px_0_rgba(0,0,0,0.03)] h-auto m-0 outline-0 p-[15px] mb-[30px] border-none opacity-50 focus:border-[2px] focus:border-solid focus:border-black opacity-100" id="state" name="user_state">
+                </div>
+
+
+                <div class="md:flex">
+                    <label for="zip_code" class="md:flex-[1_200px] md:max-w-[150px] md:pb-[30px] m-[auto_0] block mb-[8px] text-[15px] font-medium"> Zip Code </label>
+                    <input type="text" class="md:flex-[2_550px] md:max-w-[100%] md:max-w-[25%] text-[16px] w-[100%] rounded-[5px] shadow-[0_1px_0_rgba(0,0,0,0.03)] h-auto m-0 outline-0 p-[15px] mb-[30px] border-none opacity-50 focus:border-[2px] focus:border-solid focus:border-black opacity-100" id="zip_code" name="user_zip_code">
+                </div>
+                
+                <button type="submit" class="p-[19px_39px_18px_39px] text-[18px] text-center not-italic rounded-[5px] w-[100%] border-[1px] border-solid border-[1px_1px_3px] shadow-[0_-1px_0_rgba(255,255,255,0.1)] mb-[10px] mt-[25px] font-medium hover:bg-gray-400 hover:rounded-[20px] hover:transition-all"> Sign Up</button>
+            </fieldset>
+        </form>
+    </div>
+</x-header>
