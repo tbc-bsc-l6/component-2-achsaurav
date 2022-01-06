b4ea348689f4a09c37370d7a1653caf29bbe333e
saurav acharya <sau8620@gmail.com>
2022-01-06 14:02:22 +0545

    created model and migraton for database and display the products from database solving Mass Assignment Exception for products table

--- comp2/resources/views/home.blade.php
+++ comp2/resources/views/home.blade.php
@@ -7,54 +7,24 @@
                 class="uppercase text-[#ff9f1a]">Products</span> </h1>
 
         <div class="flex flex-wrap gap-[1.5rem]">
+            @foreach ($posts as $post)
+                <div
+                    class="flex-[1_1_30rem] text-center rounded-[.5rem] p-[2rem] bg-[#f9f9f9] border-[.1rem] border-solid border-[rgba(0,0,0,.1)] ">
+                    <h3 class="text-[2.5rem] text-[#2c2c54]">{{$post->title}}</h3>
+                    <h5 class="text-[2rem]">{{$post->firstname}} {{$post->surname}}</h5>
 
-            <div
-                class="flex-[1_1_30rem] text-center rounded-[.5rem] p-[2rem] bg-[#f9f9f9] border-[.1rem] border-solid border-[rgba(0,0,0,.1)] ">
-                <h3 class="text-[2.5rem] text-[#2c2c54]">Php Fundamentals</h3>
-                <h5 class="text-[2rem]">Tom Brew</h5>
-                
-                    <p class="text-[1.6rem] text-[#666] p-[1rem_0] "><span>Category:</span><a href="#" class="hover:text-blue-600 underline hover:italic">BOOK</a></p>
-                
-                <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Price: $110</p>
-                <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Number of pages: 45</p>
-                {{-- <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Fugiat, quos eum. Laborum aut a consequatur
-                        ducimus, molestias possimus quisquam rerum temporibus ipsum voluptate accusamus, unde ab
-                        asperiores?
-                    </p> --}}
-            </div>
+                    <p class="text-[1.6rem] text-[#666] p-[1rem_0] "><span>Category:</span><a href="#"
+                            class="hover:text-blue-600 underline hover:italic">BOOK</a></p>
 
-            <div
-                class="flex-[1_1_30rem] text-center rounded-[.5rem] p-[2rem] bg-[#f9f9f9] border-[.1rem] border-solid border-[rgba(0,0,0,.1)] ">
-                <h3 class="text-[2.5rem] text-[#2c2c54]">Php Fundamentals</h3>
-                <h5 class="text-[2rem]">Tom Brew</h5>
-                
-                    <p class="text-[1.6rem] text-[#666] p-[1rem_0] "><span>Category:</span><a href="#" class="hover:text-blue-600 underline hover:italic">BOOK</a></p>
-                
-                <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Price: $110</p>
-                <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Number of pages: 45</p>
-                {{-- <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Fugiat, quos eum. Laborum aut a consequatur
-                        ducimus, molestias possimus quisquam rerum temporibus ipsum voluptate accusamus, unde ab
-                        asperiores?
-                    </p> --}}
-            </div>
-
-            <div
-                class="flex-[1_1_30rem] text-center rounded-[.5rem] p-[2rem] bg-[#f9f9f9] border-[.1rem] border-solid border-[rgba(0,0,0,.1)] ">
-                <h3 class="text-[2.5rem] text-[#2c2c54]">Php Fundamentals</h3>
-                <h5 class="text-[2rem]">Tom Brew</h5>
-                
-                    <p class="text-[1.6rem] text-[#666] p-[1rem_0] "><span>Category:</span><a href="#" class="hover:text-blue-600 underline hover:italic">BOOK</a></p>
-                
-                <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Price: $110</p>
-                <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Number of pages: 45</p>
-                {{-- <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Fugiat, quos eum. Laborum aut a consequatur
-                        ducimus, molestias possimus quisquam rerum temporibus ipsum voluptate accusamus, unde ab
-                        asperiores?
-                    </p> --}}
-            </div>
+                    <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Price: ${{$post->price}}</p>
+                    <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Number of pages: {{$post->pages}}</p>
+                    {{-- <p class="text-[1.6rem] text-[#666] p-[1rem_0]">Lorem ipsum, dolor sit amet consectetur adipisicing
+                    elit. Fugiat, quos eum. Laborum aut a consequatur
+                    ducimus, molestias possimus quisquam rerum temporibus ipsum voluptate accusamus, unde ab
+                    asperiores?
+                </p> --}}
+                </div>
+            @endforeach
         </div>
 
     </section>
--- comp2/routes/web.php
+++ comp2/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',[
+        'posts'=>Post::all()
+    ]);
 });
 
 Route::get('/register',function(){
